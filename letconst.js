d0ca477c640e12553d187d5fe3f129cbc6b301b2
sadat <sadat.himel@gmail.com>
2020-08-07 11:22:55 +0600

    let,const,array declared with const,object declared with const

---
+++ letconst.js
@@ -0,0 +1,33 @@
+const hubby = "Elis Kanchon";
+// hubby = "Omar Sunny";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+// numbers = ["Sunny", "Salma", "Oit Hassan"]
+
+const nayok = {name:"sakib khan", phone:4578};
+// console.log(numbers)
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i)
+
+
+
+
+
+
+
+
+
+
+
+
