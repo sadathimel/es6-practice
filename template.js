e90c6f95b210ca2e63bfc80e0d2eb2df43bdd31d
sadat <sadat.himel@gmail.com>
2020-08-07 12:25:16 +0600

    template string, multiple line string

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+
+const multiLine = "I love you\n"
+    + "I miss you\n"
+    + "i need you";
+
+    const multiLine2 = `i love you
+    i miss you
+    no. I don't need you
+    Baily road e dorkar nai.`
+console.log(multiLine2)
